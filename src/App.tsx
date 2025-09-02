272308932611f9ce7f4e2b4df975811341ec656c
Fernando <fernandoventurini9@hotmail.com>
2025-09-02 20:49:10 +0100

    tipagem typescript  #04

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Heading } from "./components/Heading";
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 
 export function App() {
   return (
     <>
-      <Heading/>
+      <Heading>
+        Hello, world! 
+        <button>
+          <TimerIcon/>
+          </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
         aspernatur voluptatum hic ipsam vel obcaecati nostrum numquam pariatur
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,15 @@
+import type React from "react";
 import styles from "./Heading.module.css";
 
-export function Heading() {
+type HeadingProps = {
+    children: React.ReactNode;
+}
+
+export function Heading({ children }:HeadingProps) {
     return (
         <>
-            <h1 className={styles.heading}>Hello, world!</h1>
+            <h1 className={styles.heading}>                   {children}
+            </h1>
         </>
     );
 }
