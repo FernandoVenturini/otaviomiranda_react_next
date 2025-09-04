b03b60ee279295d08c7afa3dcf3e9b04fe1b03e7
Fernando <fernandoventurini9@hotmail.com>
2025-09-04 13:57:57 +0100

    creating component #21

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,33 @@
-import { Heading } from "./components/Heading";
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
-
+import { Container } from "./components/Container";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Hello, world! 
-        <button>
-          <TimerIcon/>
-          </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
-        aspernatur voluptatum hic ipsam vel obcaecati nostrum numquam pariatur
-        amet. Sit, quam. Quae porro, vel aut consequatur autem voluptate
-        nesciunt commodi voluptates? Inventore sint beatae, ipsum excepturi esse
-        culpa consectetur illo modi harum debitis cum nulla unde illum, eveniet,
-        adipisci mollitia quisquam quos. Nihil ab minima assumenda, odio
-        sapiente illum facilis deleniti fugit quis doloribus aspernatur quam
-        accusantium pariatur unde architecto? Excepturi sunt nisi magnam eveniet
-        ea, voluptatem assumenda nemo laudantium illo nulla laborum iusto! Illum
-        fugit cumque nesciunt culpa harum odio dolorum hic velit distinctio
-        suscipit ipsam non animi vero, in perferendis error. Nesciunt
-        reprehenderit animi accusantium in mollitia perferendis facilis, et
-        quidem adipisci obcaecati veritatis incidunt, voluptatem sed placeat
-        libero nulla! Amet accusamus veniam impedit sequi, quia illum qui ab ex
-        eaque enim harum nihil, maxime sit sint, sed blanditiis magni atque ut
-        similique rem vero maiores! Molestias, consequatur ullam eum sunt soluta
-        similique omnis iusto at modi culpa. Officiis eum illo explicabo, at
-        voluptatibus temporibus itaque exercitationem atque non, amet facilis
-        enim in iusto illum quisquam consequuntur voluptates aliquid ad
-        similique culpa numquam ipsa quasi repudiandae. Nulla expedita
-        doloremque perferendis recusandae doloribus ad dolorem, suscipit
-        accusantium debitis laborum ratione tempore et pariatur nisi iste unde
-        consectetur quas, numquam ut. Perferendis temporibus ut quod similique
-        unde, quidem nobis velit, explicabo qui possimus laudantium aut
-        reiciendis delectus. Quo placeat ab nobis dolore ratione nam sequi quis.
-        Soluta eius corporis laborum consequatur voluptatum id omnis ducimus.
-        Eos numquam aut molestias voluptas fuga error, dolores, et magni iusto
-        quam nam, officiis explicabo laborum beatae excepturi commodi
-        consequatur dignissimos. Enim minima sit libero reprehenderit
-        accusantium illo autem, iste praesentium commodi voluptas corporis ex
-        aliquam mollitia delectus quo dicta excepturi dolores error sapiente,
-        ducimus sint? Animi nesciunt tenetur harum possimus illo, cumque
-        incidunt laudantium?
-      </p>
+        <Container>
+          <section>
+            LOGO
+          </section>
+        </Container>
+
+        <Container>
+          <section>
+            MENU
+          </section>
+        </Container>
+
+        <Container>
+          <section>
+            FORM
+          </section>
+        </Container>
+
+        <Container>
+          <section>
+            FOOTER
+          </section>
+        </Container>
     </>
   );
 }
