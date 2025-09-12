1fa01c44eed35a195faea7bae8a778c1220d9b3a
Fernando <fernandoventurini9@hotmail.com>
2025-09-12 09:35:13 +0100

    using pure functions without side effects #37

--- src/components/DefaultButton/index.tsx
+++ src/components/DefaultButton/index.tsx
@@ -5,11 +5,15 @@ type DefaultButtonProps = {
     color?: 'green' | 'red';
 } & React.ComponentProps<'button'>;
 
+<<<<<<< HEAD
 export function DefaultButton({
     icon, 
     color='green', 
     ...props}: DefaultButtonProps) {
 
+=======
+export function DefaultButton({icon, color='green', ...props}: DefaultButtonProps) {
+>>>>>>> c7f3d88 (insering dark theme #36)
     return (
         <>
             <button className={`${styles.button} ${styles[color]}`} {...props}>
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,62 @@
 import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
-    return (
-        <>
-            <nav className={styles.menu}>
-                <a className={styles.menuLink} href="#">
-                    <HouseIcon />
-                </a>
-                <a className={styles.menuLink} href="#">
-                    <HistoryIcon />
-                </a>
-                <a className={styles.menuLink} href="#">
-                    <SettingsIcon />
-                </a>
-                <a className={styles.menuLink} href="#">
-                    <SunIcon />
-                </a>
-            </nav>
-        </>
-    );
-} 
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const newTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return newTheme;
+    });
+
+    document.documentElement.setAttribute('data-theme', theme);
+  };
+
+  return (
+    <>
+      <nav className={styles.menu}>
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Ir para a Home"
+          title="Ir para a Home"
+        >
+          <HouseIcon />
+        </a>
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Ver historico"
+          title="Ver historico"
+        >
+          <HistoryIcon />
+        </a>
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Configuracoes"
+          title="Configuracoes"
+        >
+          <SettingsIcon />
+        </a>
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Mudar Tema"
+          title="Mudar Tema"
+          onClick={handleThemeChange}
+        >
+          <SunIcon />
+        </a>
+      </nav>
+    </>
+  );
+}
