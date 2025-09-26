27b1e82fa59bce166f81107d74ea721f4b7bca64
Fernando <fernandoventurini9@hotmail.com>
2025-09-26 22:54:32 +0100

    global application status #44

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,39 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
 //import { NotFound } from './pages/NotFound';
 //import { AboutPomodoro } from './pages/AboutPomodoro';
 
 
+// export type TaskStateModel = {
+//   tasks: TaskModel[];
+//   secondsRemaining: number;
+//   formattedSecondsRemaining: string;
+//   activeTask: number;
+//   config: {
+//     workTime: number;
+//     shortBreakTime: number;
+//     longBreakTime: number;
+//   };
+// };
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: 0,
+  currentCycle: 0,
+  config: {
+    workTime: 25 * 60,
+    shortBreakTime: 5 * 60,
+    longBreakTime: 15 * 60,
+  },
+}
+
 export function App() {
+  const [state, setState] = useState(initialState);
   
   return <Home/>;
 
